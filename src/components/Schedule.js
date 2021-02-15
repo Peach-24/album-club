f46d9ee554c9bbb808296b223c5ecfac0430b2c1
Josh Peachey <joshpeachey24@gmail.com>
2021-02-15 18:39:03 +0000

    Fetches submissions albums from firestore

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import firebase from "firebase";
+import "firebase/firebase-firestore";
 
 export default function Schedule() {
+  const [albums, setAlbums] = useState([]);
+
+  const fetchAlbums = () => {
+    const db = firebase.firestore();
+    db.collection("submissions")
+      .get()
+      .then((querySnapshot) => {
+        let albumsList = [];
+        querySnapshot.forEach((doc) => {
+          albumsList.push(doc.data());
+        });
+        setAlbums(albumsList);
+      });
+  };
+
+  // useEffect(() => {}, []);
+
   return (
     <div>
       <h4>Schedule</h4>
+      <button onClick={() => fetchAlbums()}>Fetch albums</button>
+      <button onClick={() => console.log(albums)}>View albums in state</button>
     </div>
   );
 }
