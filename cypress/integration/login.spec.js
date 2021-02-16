b01a506350eb4921bae5bae45e75f5607b2b1c58
Josh Peachey <joshpeachey24@gmail.com>
2021-02-16 11:12:55 +0000

    Adds baseUrl to cypress config + minor login css change

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,6 +1,6 @@
 describe("Login page on load", () => {
   it("has all of the elements", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
     cy.get(".password-login-container");
     cy.get(".password-login-box");
     cy.get("#login-logo");
@@ -9,19 +9,19 @@ describe("Login page on load", () => {
     cy.get("#error-msg-text");
   });
   it("Can enter a password", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
     cy.get("#password-login")
       .type("sheffhampton")
       .should("have.value", "sheffhampton");
   });
   it("Clicking button with incorrect password shows the error message", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
     cy.get("#password-login").type("wrongpassword");
     cy.get("#login-button").click();
     cy.get("#error-msg-text").contains("wrong password...");
   });
   it("Clicking button with correct password navigates to a new url", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
     cy.get("#password-login").type("sheffhampton");
     cy.get("#login-button").click();
     cy.url().should("include", "/home");
