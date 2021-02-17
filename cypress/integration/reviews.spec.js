2bac43300aca6682076ba701bab54877670cef8b
Josh Peachey <joshpeachey24@gmail.com>
2021-02-17 15:03:41 +0000

    Adds basic tests for Reviews screen

--- cypress/integration/reviews.spec.js
+++ cypress/integration/reviews.spec.js
@@ -1,3 +1,29 @@
-describe("", () => {
-  it("", () => {});
+describe("Reviews screen", () => {
+  it("displays login screen if user hasn't entered password", () => {
+    cy.visit("/reviews");
+    cy.get(".password-login-container");
+    cy.get(".password-login-box");
+    cy.get("#login-logo");
+    cy.get("#password-login");
+    cy.get("#login-button");
+    cy.get("#error-msg-text");
+  });
+  it("Lands on reviews after logging in", () => {
+    cy.visit("/schedule");
+    cy.get("#password-login").type("sheffhampton");
+    cy.get("#login-button").click();
+    cy.url().should("include", "/reviews");
+  });
+  it("has all of the core elements", () => {
+    cy.get("nav");
+    cy.get("#reviews-container");
+    cy.get("#reviews-box");
+    cy.get("#reviews-list");
+    cy.get("#reviews-list-item");
+  });
 });
+
+/* 
+Has certain number of li in ol
+cy.get('.left-nav>.nav').children().should('have.length', 8)
+*/
