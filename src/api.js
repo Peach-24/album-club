f1fc35f1de81ccab6af35f1a2e39db68b0280749
Josh Peachey <joshpeachey24@gmail.com>
2021-02-22 21:31:55 +0000

    Deployed to Netlify - need to fix CORS issue

--- src/api.js
+++ src/api.js
@@ -1,12 +1,23 @@
 import axios from "axios";
 
 const spotifyOembed = axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://open.spotify.com",
+  baseURL: "https://open.spotify.com",
 });
 
+// https://open.spotify.com/oembed?url=https://open.spotify.com/album/42kVfUV56KG0Ao8EbqaX51?si=hYxnlfxASdumXzgOfk1Vng
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+  mode: "no-cors",
+};
+
 export const fetchArtworkURL = (albumLink) => {
-  return spotifyOembed.get(`/oembed?url=${albumLink}`).then((res) => {
-    console.log(res.data.thumbnail_url);
-    return res.data.thumbnail_url;
-  });
+  // axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+  return axios
+    .get(`https://open.spotify.com/oembed?url=${albumLink}`, { headers })
+    .then((res) => {
+      console.log(res.data.thumbnail_url);
+      return res.data.thumbnail_url;
+    });
 };
